3970e5e73249e8abc358b02b829cda2bbde76e77
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-11-09 16:59:54 +0200

    feat: footer markup is ready. No responsive

---
+++ src/blocks/modules/footer/footer.js
@@ -0,0 +1,20 @@
+const btnCollapse = document.querySelector(".main-footer .js-btn-collapse");
+
+if (btnCollapse) {
+  btnCollapse.addEventListener("click", function (e) {
+    e.preventDefault();
+    const cats = document.querySelector(".main-footer__cats-inner");
+
+    this.classList.toggle("active");
+
+    if (this.classList.contains("active")) {
+      this.querySelector("span").innerText = this.dataset.hideText;
+    } else {
+      this.querySelector("span").innerText = this.dataset.showText;
+    }
+
+    if (cats) {
+      cats.classList.toggle("active");
+    }
+  });
+}
